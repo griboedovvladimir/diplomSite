1f56de03c271660048947b463a299b1dafbbc6c9
likecoffee <likecoffee@yandex.ru>
2017-09-12 09:35:49 +0300

    Added project folders

---
+++ js/script.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){
+
+    $(function () {
+        // инициализировать все элементы на страницы, имеющих атрибут data-toggle="tooltip", как компоненты tooltip
+        $('[data-toggle="tooltip"]').tooltip()
+    });
+
+    $('#event1').
+    mouseover(function(){
+        $('#page1').removeClass('stiker').addClass('stikerin');
+    }).
+    mouseout(function(){
+        // при покидании элемента
+        $('#page1').removeClass('stikerin').addClass('stiker');
+    });
+
+
+$('#event2').
+mouseover(function(){
+    $('#page2').removeClass('stiker').addClass('stikerin2');
+}).
+mouseout(function(){
+    // при покидании элемента
+    $('#page2').removeClass('stikerin2').addClass('stiker');
+});
+
+    $('#event3').
+    mouseover(function(){
+        $('#page3').removeClass('stiker').addClass('stikerin3');
+    }).
+    mouseout(function(){
+        // при покидании элемента
+        $('#page3').removeClass('stikerin3').addClass('stiker');
+    });
+
+    $('#event4').
+    mouseover(function(){
+        $('#page4').removeClass('stiker').addClass('stikerin4');
+    }).
+    mouseout(function(){
+        // при покидании элемента
+        $('#page4').removeClass('stikerin4').addClass('stiker');
+    });
+
+    $('#event5').
+    mouseover(function(){
+        $('#page5').removeClass('stiker').addClass('stikerin5');
+    }).
+    mouseout(function(){
+        // при покидании элемента
+        $('#page5').removeClass('stikerin5').addClass('stiker');
+    });
+});
